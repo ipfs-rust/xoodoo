1bb91457cef0d8452c58ac59a138d201ef96285f
David Craven <david@craven.ch>
2021-04-16 11:20:48 +0200

    Cleanup xoodyak.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -4,17 +4,17 @@ use xoodyak::*;
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Xoodoo permutation", |b| {
         let mut out = [0u8; 48];
-        let mut st = Xoodoo::default();
+        let mut st = Xoodoo::<1, 12>::default();
         b.iter(|| {
             st.permute();
-            st.bytes(&mut out);
+            st.extract_bytes(0, &mut out);
             out
         })
     });
 
     c.bench_function("Xoodyak hash", |b| {
         let mut out = [0u8; 64];
-        let mut st = XoodyakHash::new();
+        let mut st = Xoodyak::hash();
         b.iter(|| {
             st.absorb(b"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ");
             st.squeeze(&mut out);
@@ -24,7 +24,7 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     c.bench_function("Xoodyak keyed", |b| {
         let mut out = [0u8; 64];
-        let mut st = XoodyakKeyed::new(b"key", None,None, None).unwrap();
+        let mut st = Xoodyak::keyed(b"key", None, None, None);
         b.iter(|| {
             st.absorb(b"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ");
             st.squeeze(&mut out);
--- src/xoodoo/mod.rs
+++ src/xoodoo/mod.rs
@@ -2,85 +2,75 @@ use rawbytes::RawBytes;
 use zeroize::Zeroize;
 
 #[cfg(not(target_arch = "x86_64"))]
-mod impl_portable;
+mod impl_portable_x1;
 #[cfg(target_arch = "x86_64")]
-mod impl_x86_64;
+mod impl_x86_64_x1;
 
 const ROUND_KEYS: [u32; 12] = [
-    0x058, 0x038, 0x3c0, 0x0d0, 0x120, 0x014, 0x060, 0x02c, 0x380, 0x0f0, 0x1a0, 0x012,
+    0x012, 0x1a0, 0x0f0, 0x380, 0x02c, 0x060, 0x014, 0x120, 0x0d0, 0x3c0, 0x038, 0x058,
 ];
 
-#[derive(Clone, Debug, Default)]
+/// Xoodoo permutation parameterized over the number of states and the number of rounds. Typical
+/// R values are 6 for xoofff and 12 for xoodyak, and typical values for N would be 1 for xoodyak
+/// and depending on if the cpu supports 128/256/512 vector instructions 4/8/16.
+#[derive(Clone, Debug)]
 #[repr(C)]
-pub struct Xoodoo {
-    st: [u32; 12],
+pub struct Xoodoo<const N: usize, const R: usize> {
+    st: [[u32; 12]; N],
 }
 
-impl Xoodoo {
-    #[inline(always)]
-    fn bytes_view(&self) -> &[u8] {
-        let view = RawBytes::bytes_view(&self.st);
+impl<const N: usize, const R: usize> Default for Xoodoo<N, R> {
+    fn default() -> Self {
+        Self { st: [[0; 12]; N] }
+    }
+}
+
+impl<const N: usize, const R: usize> Xoodoo<N, R> {
+    fn bytes_view(&self, i: usize) -> &[u8] {
+        let view = RawBytes::bytes_view(&self.st[i]);
         debug_assert_eq!(view.len(), 48);
         view
     }
 
-    #[inline(always)]
-    fn bytes_view_mut(&mut self) -> &mut [u8] {
-        let view = RawBytes::bytes_view_mut(&mut self.st);
+    fn bytes_view_mut(&mut self, i: usize) -> &mut [u8] {
+        let view = RawBytes::bytes_view_mut(&mut self.st[i]);
         debug_assert_eq!(view.len(), 48);
         view
     }
 
-    #[inline(always)]
     fn endian_swap(&mut self) {
-        for word in self.st.iter_mut() {
-            *word = (*word).to_le()
+        for state in self.st.iter_mut() {
+            for word in state.iter_mut() {
+                *word = (*word).to_le();
+            }
         }
     }
 
-    #[inline]
-    pub fn from_bytes(bytes: [u8; 48]) -> Self {
-        let mut st = Xoodoo::default();
-        let st_bytes = st.bytes_view_mut();
-        st_bytes.copy_from_slice(&bytes);
-        st
+    pub fn zeroize(&mut self) {
+        for i in 0..N {
+            self.st[i].zeroize()
+        }
     }
 
-    #[inline(always)]
-    pub fn bytes(&self, out: &mut [u8; 48]) {
-        let st_bytes = self.bytes_view();
-        out.copy_from_slice(st_bytes);
-    }
-
-    #[inline(always)]
-    pub fn add_byte(&mut self, byte: u8, offset: usize) {
+    pub fn add_bytes(&mut self, i: usize, bytes: &[u8], offset: usize) {
         self.endian_swap();
-        let st_bytes = self.bytes_view_mut();
-        st_bytes[offset] ^= byte;
-        self.endian_swap();
-    }
-
-    #[inline(always)]
-    pub fn add_bytes(&mut self, bytes: &[u8]) {
-        self.endian_swap();
-        let st_bytes = self.bytes_view_mut();
-        for (st_byte, byte) in st_bytes.iter_mut().zip(bytes) {
+        let st_bytes = self.bytes_view_mut(i);
+        for (st_byte, byte) in st_bytes.iter_mut().skip(offset).zip(bytes) {
             *st_byte ^= byte;
         }
         self.endian_swap();
     }
 
-    #[inline(always)]
-    pub fn extract_bytes(&mut self, out: &mut [u8]) {
+    pub fn extract_bytes(&mut self, i: usize, out: &mut [u8]) {
         self.endian_swap();
-        let st_bytes = self.bytes_view();
+        let st_bytes = self.bytes_view(i);
         out.copy_from_slice(&st_bytes[..out.len()]);
         self.endian_swap();
     }
 }
 
-impl Drop for Xoodoo {
+impl<const N: usize, const R: usize> Drop for Xoodoo<N, R> {
     fn drop(&mut self) {
-        self.st.zeroize()
+        self.zeroize()
     }
 }
